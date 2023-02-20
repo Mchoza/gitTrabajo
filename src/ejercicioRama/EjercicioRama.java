c2eb6e5fffd14b2549bcea6729b86711e53597b7
Mery <mchozamerino@gmail.com>
2023-02-20 10:21:37 +0100

    primer commit

---
+++ src/ejercicioRama/EjercicioRama.java
@@ -0,0 +1,12 @@
+package ejercicioRama;
+
+public class EjercicioRama {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Ha nacido una estrella");
+
+	}
+
+}
